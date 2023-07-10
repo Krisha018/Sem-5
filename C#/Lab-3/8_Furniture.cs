b7dda00893a1a0dd194b4022ac338c98cf79a5dc
Padaliya Krisha <108071979+Krisha018@users.noreply.github.com>
2023-07-11 00:17:45 +0530

    Add files via upload

---
+++ C#/Lab-3/8_Furniture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+   public class Furniture
+    {
+       public  String Material;
+        public double Price;
+
+    }
+   public class Table : Furniture
+    {
+        public double Height;
+        public double SurfaceArea;
+
+        public Table(String Material, double Price, double Height, double SurfaceArea)
+        {
+            this.Material = Material;
+            this.Price = Price;
+            this.Height = Height;
+            this.SurfaceArea = SurfaceArea;
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Material Type : " + Material);
+            Console.WriteLine("Price : " + Price);
+            Console.WriteLine("Height : " + Height);
+            Console.WriteLine("Surface Area : " + SurfaceArea);
+        }
+    }
+
+  public  class Demo
+    {
+     public   static void Main(string[] args)
+        {
+            Table t = new Table("Material", 50000, 120, 200);
+            t.displayData();
+
+        }
+    }
+}
---
+++ C#/Lab-3/9_Account_Details.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    public class Account_Details
+    {
+        public int AccountNumber;
+        public String UserName;
+        public String Email;
+        public String Type;
+        public double Balance;
+
+        public Account_Details()
+        {
+            Console.WriteLine("Enter Account Number : ");
+            this.AccountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter User Name : ");
+            this.UserName = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            this.Email = Console.ReadLine();
+            Console.WriteLine("Enter Type : ");
+            this.Type = Console.ReadLine();
+            Console.WriteLine("Enter Balance : ");
+            this.Balance = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+  public  class Calculate : Account_Details
+    {
+        public double TimePeriod;
+        public double RateOfIntrest;
+
+        public Calculate()
+            : base()
+        {
+            Console.WriteLine("Enter Time Period : ");
+            this.TimePeriod = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Rate Of Intrest : ");
+            this.RateOfIntrest = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void calculateROI()
+        {
+            Console.WriteLine("ROI : " + (Balance * RateOfIntrest * TimePeriod) / 100);
+        }
+    }
+
+  public  class Demo
+    {
+     public   static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            c.calculateROI();
+
+
+        }
+    }
+}
